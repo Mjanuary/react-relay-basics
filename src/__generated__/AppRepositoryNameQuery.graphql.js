bc832820824e4952ee800cbeb6bfce881f1f20a5
MUHAWENIMANA Janvier <janviermuhawenimana@gmail.com>
2022-09-04 18:20:29 +0200

    first commit

---
+++ src/__generated__/AppRepositoryNameQuery.graphql.js
@@ -0,0 +1,109 @@
+/**
+ * @generated SignedSource<<28d2011512ad80866eb7cc6ea1c41adb>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+var node = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "AppRepositoryNameQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "Project",
+        "kind": "LinkedField",
+        "name": "projects",
+        "plural": true,
+        "selections": [
+          (v0/*: any*/),
+          (v1/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "Client",
+            "kind": "LinkedField",
+            "name": "client",
+            "plural": false,
+            "selections": [
+              (v1/*: any*/)
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "RootQueryType",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "AppRepositoryNameQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "Project",
+        "kind": "LinkedField",
+        "name": "projects",
+        "plural": true,
+        "selections": [
+          (v0/*: any*/),
+          (v1/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "Client",
+            "kind": "LinkedField",
+            "name": "client",
+            "plural": false,
+            "selections": [
+              (v1/*: any*/),
+              (v0/*: any*/)
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "7aaa4d7e523fb8afde7e2d23f48b86a9",
+    "id": null,
+    "metadata": {},
+    "name": "AppRepositoryNameQuery",
+    "operationKind": "query",
+    "text": "query AppRepositoryNameQuery {\n  projects {\n    id\n    name\n    client {\n      name\n      id\n    }\n  }\n}\n"
+  }
+};
+})();
+
+node.hash = "d2a957ebb824337a9cdedf29a60e3a4d";
+
+module.exports = node;
